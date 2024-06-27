87cf7006220bab79eb4e2164c9becb0c9b175775
Erez <erezur5@gmail.com>
2024-06-27 16:12:11 +0300

    added filter by url and pagination

--- models/products.model.js
+++ models/products.model.js
@@ -25,5 +25,20 @@ const productSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+// Static method to find products by price range
+productSchema.statics.findProductsByPriceRange = async function (
+  minPrice,
+  maxPrice
+) {
+  try {
+    const products = await this.find({
+      price: { $gte: minPrice, $lte: maxPrice },
+    });
+    return products;
+  } catch (error) {
+    throw new Error("Error finding products by price range: " + error.message);
+  }
+};
+
 const Product = mongoose.model("Product", productSchema);
 module.exports = Product;
